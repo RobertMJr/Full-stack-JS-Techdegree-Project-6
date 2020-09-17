3f4f8145a72c58a3396fb76ca76e21553983215b
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-09-17 14:55:06 +0300

    WIP

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use((err, req, res, next) => {
 });
 
 /* Listen on port 3000, and log a string to the console to idnicate on which port the app is listening to. */
-const server = app.listen(process.env.PORT || 3000, ()=> {
-    let port = server.address().port;
-    console.log(`The application is running on localhost: ${port}`);
+app.listen(3000, ()=> {
+    console.log('The application is running on localhost: 3000');
 });
